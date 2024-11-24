05967413377c3fc095c66ccd6d00a2bd29f44d33
Francesco Gorgone <148762861+francescogorgone@users.noreply.github.com>
2024-11-24 12:34:32 +0100

    Create funzioni.py

---
+++ gentle_introduction/funzioni.py
@@ -0,0 +1,11 @@
+#definisco funzione che calcola il prodotto tra due numeri
+
+def prodotto_tra_due_numeri(a,b):
+    c = a*b
+    return c
+
+res1 = prodotto_tra_due_numeri(3,4)
+res2 = prodotto_tra_due_numeri(5,6)
+
+print(res1)
+print(res2)
